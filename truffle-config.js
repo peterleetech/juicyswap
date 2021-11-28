2fa1cad640c09f822992fe716bfeb684a7d4b421
Peter Lee <shiwaitaoyuan427@gmail.com>
2021-11-27 22:13:02 -0800

    BSC testnet

--- truffle-config.js
+++ truffle-config.js
@@ -3,6 +3,9 @@ require('babel-polyfill');
 require('dotenv').config();
 const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
 const privateKeys = process.env.PRIVATE_KEYS || ""
+const BSCHDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   networks: {
@@ -21,7 +24,22 @@ module.exports = {
       gas: 5000000,
       gasPrice: 25000000000,
       network_id: 4
-    }
+    },
+    testnet: {
+      provider: () => new BSCHDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      networkCheckTimeout: 1000000,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsc: {
+      provider: () => new BSCHDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
   plugins: [
     'truffle-plugin-verify'
